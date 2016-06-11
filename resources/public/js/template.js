311cc19aa4ebe9ef1fdd43e7e736b0c67c9ac184
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2016-06-11 11:30:56 +0200

    JS aestethics

--- resources/public/js/template.js
+++ resources/public/js/template.js
@@ -1,7 +1,7 @@
 (function() {
-  var message = "I got loaded!";
-  document.addEventListener("DOMContentLoaded", function(event) {
-  if (console) {
+  var message = 'I got loaded!';
+  document.addEventListener('DOMContentLoaded', function(event) {
+    if (console) {
       console.log(message);
     } else {
       alert(message);
